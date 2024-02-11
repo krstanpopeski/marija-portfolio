62b9e4518d8b09af0e493091c4223c4257e1538c
marijapopeska <108872423+marijapopeska@users.noreply.github.com>
2024-02-11 13:30:59 +0100

    Implementing new layout and visual elements

--- js/main.js
+++ js/main.js
@@ -1,12 +1,17 @@
-document.addEventListener("DOMContentLoaded", function () {
-    // Smooth scrolling for anchor links
-    document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-        anchor.addEventListener('click', function (e) {
-            e.preventDefault();
-
-            document.querySelector(this.getAttribute('href')).scrollIntoView({
-                behavior: 'smooth'
-            });
-        });
-    });
+$(window).scroll(function () {
+  $(".top").css("opacity", 1 - $(window).scrollTop() / 500);
 });
+
+// Get the current year
+const currentYear = new Date().getFullYear();
+
+// Update the HTML content with the current year
+document.querySelector('.currentYear').textContent = `© ${currentYear}. All Rights Reserved to Marija Popeska.`;
+
+const activePage = window.location.pathname;
+console.log(activePage)
+const navLinks = document.querySelectorAll('nav a').forEach(link => {
+  if (link.href.includes(`${activePage}`)) {
+    link.classList.add('active')
+  }
+})
