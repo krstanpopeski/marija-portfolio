309e311f4ed41c085e119dab934d8bda0022079a
marijapopeska <108872423+marijapopeska@users.noreply.github.com>
2024-02-16 13:07:47 +0100

    optimizing images and adjusting styles

--- js/main.js
+++ js/main.js
@@ -8,10 +8,16 @@ const currentYear = new Date().getFullYear();
 // Update the HTML content with the current year
 document.querySelector('.currentYear').textContent = `© ${currentYear}. All Rights Reserved to Marija Popeska.`;
 
-const activePage = window.location.pathname;
-console.log(activePage)
-const navLinks = document.querySelectorAll('nav a').forEach(link => {
-  if (link.href.includes(`${activePage}`)) {
-    link.classList.add('active')
-  }
-})
+const fullscreenableContainers = document.querySelectorAll('.fullscreen-container');
+
+// Function to toggle fullscreen mode
+function toggleFullscreen(element) {
+  element.classList.toggle('fullscreen');
+}
+
+// Add click event listener to each fullscreenable container
+fullscreenableContainers.forEach(function (container) {
+  container.addEventListener('click', function () {
+    toggleFullscreen(this);
+  });
+});
